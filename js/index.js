9d38d9cf67c2721e3a17ace5d94bbfd5fe234da6
mimo <877618188@qq.com>
2017-06-30 15:25:00 +0800

    myfirst

---
+++ js/index.js
@@ -0,0 +1,187 @@
+$(function(){
+    $("body").animate({width:"100%"},1000);
+    $(window).on('scroll', function () {
+        var scrollTop = $('body').scrollTop();
+        if (scrollTop > 200) {
+            $('#up').css({'display':"block"});
+        }else {
+            $('#up').css({'display':"none"});
+        }
+    });
+
+    $('#up').on('click', function(){
+        $('body').animate({scrollTop:0},500);
+    });
+
+
+
+    $(".skill span").hover(function(){
+        $(this).css({"transform":"scale(1.3)"},1000)
+    },function(){
+        $(this).css({"transform":"scale(1)"},1000)
+    })
+
+
+
+$('#introduce').css({height:0}).animate({height:300},2000);
+
+
+ var slide11=$(".slide11");    
+    for(var i=0;i<4;i++){
+         var deg=i*90;
+         $("<div class='item11'></div>")
+         .css({
+              "background-image":"url(images/"+i+".jpg)",
+              "transform":"rotateX("+deg+"deg) translateZ(72px)" 
+          })
+         .appendTo(slide11);
+    }
+    
+  
+    
+    var num=0; 
+    var int=self.setInterval(function(){clock()},2000);
+    function clock(){
+        num++;      
+        $(".item11").each(function(index, element) {            
+            var deg=index*90-num*90;           
+            $(this).css({
+                "transform":"rotateX("+deg+"deg) translateZ(72px)" 
+            })
+    })
+        
+   }
+
+
+
+    var slide2=$(".slide2");    
+  
+  for(var i=0;i<4;i++){
+     var deg2=i*90;
+     $("<div class='item2'></div>")
+     .appendTo(slide2);
+  }
+  
+  var cou=5;
+  var w2=slide2.width()/cou;
+  $(".item2").each(function(index, element) { 
+    var deg2=index*90;
+    for(var i=0;i<cou;i++){  
+      var x2=i*w2;
+      $("<div class='pic2'></div>")
+      .width(w2)
+      .css({
+          "left":x2+"px",
+        "background-image":"url(images/"+index+".jpg)",
+        "background-position":-x2+"px 0px",
+        "transform":"rotateX("+deg2+"deg) translateZ(72px)" 
+      })
+        .appendTo(this)
+    }
+        
+    });
+  
+  
+  var n=0;
+  var int2=self.setInterval(function(){clock2()},2000);
+  function clock2(){
+    n++;  
+    $(".item2").each(function(index, element) {
+        
+        var deg2=index*90-n*90;
+        
+        $(this).find(".pic2").each(function(i, element) {
+          
+          var delay=i*0.15;
+          
+          $(this).css({
+               "transition":"transform .5s "+delay+"s",
+              "transform":"rotateX("+deg2+"deg) translateZ(72px)"
+          });
+        });
+      });  
+  }
+
+
+
+
+        var oPic=$("#pic");
+        for(var i=0;i<25;i++) {
+            var x = i % 5;
+            var y = parseInt(i / 5);
+            $("<i class='item3'></i>").css({
+                "left": x * 60 + "px",
+                "top": y * 30.6 + "px",
+                "background-position": x * (-60) + "px " + y * (-30.6) + "px"
+            }).appendTo(oPic);
+        }
+    var b=false;
+       
+
+    oPic.click(function(){
+        if(b==false){
+        $("i").each(function(){  
+          var  range=3000  
+          var  rx=Math.random()*range*2-range;
+          var  ry=Math.random()*range*2-range;
+          $(this).css({
+            "transform":"translate("+rx+"px,"+ry+"px)",
+            "opacity":0
+        });
+                b=true;
+            })
+        }else{
+           
+            $("i").each(function (index,element) {
+            $(this).css({
+            "transform":"translate(0,0)",
+          "opacity":1  
+       })
+                b=false;
+            })
+        }
+        })
+
+
+
+
+
+
+  $("#navb").click(function(){
+     $('html,body').animate({scrollTop:$("#b").offset().top},1000)
+  })
+  $("#navc").click(function(){
+     $('html,body').animate({scrollTop:$("#c").offset().top},1000)
+  })
+  $("#navd").click(function(){
+    $('html,body').animate({scrollTop:$("#d").offset().top},1500)
+  })
+  $("#nave").click(function(){
+    $('html,body').animate({scrollTop:$("#e").offset().top},1500)
+  })
+  $("#top").click(function(){
+     $('html,body').animate({scrollTop:$("#navtop").offset().top},1500)
+  })
+});
+
+
+
+
+
+
+
+
+ 
+   
+
+
+
+
+
+
+
+
+
+
+
+
