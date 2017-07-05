13d5ba08bc1861c7fe9c1eeb7959a6b38fcaffd8
mimo0921 <877618188@qq.com>
2017-07-06 06:15:35 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -26,6 +26,19 @@ $(function(){
 $('#introduce').css({height:0}).animate({height:300},2000);
 
 
+
+function browserRedirect() {
+    var sUserAgent = navigator.userAgent.toLowerCase();
+    var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+    var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+    var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+    var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+    var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+    var bIsAndroid = sUserAgent.match(/android/i) == "android";
+    var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+    var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+    if (!(bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) ){
+        
  var slide11=$(".slide11");    
     for(var i=0;i<4;i++){
          var deg=i*90;
@@ -54,8 +67,7 @@ $('#introduce').css({height:0}).animate({height:300},2000);
 
 
 
-    var slide2=$(".slide2");    
-  
+  var slide2=$(".slide2");    
   for(var i=0;i<4;i++){
      var deg2=i*90;
      $("<div class='item2'></div>")
@@ -142,7 +154,9 @@ $('#introduce').css({height:0}).animate({height:300},2000);
         }
         })
 
-
+    }
+}
+browserRedirect();
 
 
 
@@ -165,6 +179,125 @@ $('#introduce').css({height:0}).animate({height:300},2000);
 
 
 
+//pc端动效修改
+var slide11=$(".slide11");    
+    for(var i=0;i<4;i++){
+         var deg=i*90;
+         $("<div class='item11'></div>")
+         .css({
+              "background-image":"url(images/"+i+".jpg)",
+              "transform":"rotateX("+deg+"deg) translateZ(60px)" 
+          })
+         .appendTo(slide11);
+     }
+    
+    
+  var num=0; 
+  var int=self.setInterval(function(){clock()},2000);
+  function clock(){
+      num++;      
+      $(".item11").each(function(index, element) {            
+          var deg=index*90-num*90;           
+          $(this).css({
+              "transform":"rotateX("+deg+"deg) translateZ(60px)" 
+          })
+     })
+      
+  }
+
+
+
+    var slide2=$(".slide2");    
+  
+  for(var i=0;i<4;i++){
+     var deg2=i*90;
+     $("<div class='item2'></div>")
+     .appendTo(slide2);
+  }
+  
+  var cou=5;
+  var w2=slide2.width()/cou;
+  $(".item2").each(function(index, element) { 
+    var deg2=index*90;
+    for(var i=0;i<cou;i++){  
+      var x2=i*w2;
+      $("<div class='pic2'></div>")
+      .width(w2)
+      .css({
+          "left":x2+"px",
+        "background-image":"url(images/"+index+".jpg)",
+        "background-position":-x2+"px 0px",
+        "transform":"rotateX("+deg2+"deg) translateZ(60px)" 
+      })
+        .appendTo(this)
+    }
+        
+    });
+  
+  
+  var n=0;
+  var int2=self.setInterval(function(){clock2()},2000);
+  function clock2(){
+    n++;  
+    $(".item2").each(function(index, element) {
+        
+        var deg2=index*90-n*90;
+        
+        $(this).find(".pic2").each(function(i, element) {
+          
+          var delay=i*0.15;
+          
+          $(this).css({
+               "transition":"transform .5s "+delay+"s",
+              "transform":"rotateX("+deg2+"deg) translateZ(60px)"
+          });
+        });
+      });  
+  }
+
+
+
+
+        var oPic=$("#pic");
+        for(var i=0;i<25;i++) {
+            var x = i % 5;
+            var y = parseInt(i / 5);
+            $("<i class='item3'></i>").css({
+                "left": x * 47.8 + "px",
+                "top": y * 23.8 + "px",
+                "background-position": x * (-47.8) + "px " + y * (-23.8) + "px"
+            }).appendTo(oPic);
+        }
+    var b=false;
+       
+
+    oPic.click(function(){
+        if(b==false){
+        $("i").each(function(){  
+          var  range=3000  
+          var  rx=Math.random()*range*2-range;
+          var  ry=Math.random()*range*2-range;
+          $(this).css({
+            "transform":"translate("+rx+"px,"+ry+"px)",
+            "opacity":0
+        });
+                b=true;
+            })
+        }else{
+           
+            $("i").each(function (index,element) {
+            $(this).css({
+            "transform":"translate(0,0)",
+          "opacity":1  
+       })
+                b=false;
+            })
+        }
+        })
+
+
+
+
 
    var index=0;
     var word1=$("#w0").html();
@@ -180,11 +313,6 @@ $('#introduce').css({height:0}).animate({height:300},2000);
          
     }
     setInterval(type, 300);
-
-
-
-
-
 
 });
 
